3993688717c937a11e3faa8081a8f91ce41f3821
epicary22 <89352235+epicary22@users.noreply.github.com>
2022-10-02 13:12:20 -0500

    Improve PletLangMessage.to_phonetics() Remove unnecessary code

--- plet_lang_message.py
+++ plet_lang_message.py
@@ -6,45 +6,47 @@ BRACKETS = ("{", "}")
 with open("charsets/shorthand_0b.json", encoding="utf8") as file:
     phon_chars = json.load(file)
 
+
 class PletLangMessage:
 
-    def __init__(self, msg: str):
+    def __init__(self, msg: str) -> None:
         self.msg = msg
-        self.__pmsg = self.msg  # Pletlang message
-        self.__pmsg = self.__pmsg.replace("   ", "TEMP")
-        self.__pmsg = self.__pmsg.replace(" ", "")
-        self.__pmsg = self.__pmsg.replace("TEMP", " ")
-        print(self.__pmsg)
-        self.__message_layers = []
-        self.fmsg = ""  # Fonetic message
+        self.pmsg = self.msg  # Pletlang message
+        self.format_pmsg()
+        self.fmsg = ""  # Fonetic messages
 
-    def to_phonetics(self) -> str:
+    def format_pmsg(self) -> None:
+        self.pmsg = self.pmsg.replace("   ", "TEMP")
+        self.pmsg = self.pmsg.replace(" ", "")
+        self.pmsg = self.pmsg.replace("TEMP", " ")
+
+    def get_phonetics(self) -> str:
+        if not self.fmsg:
+            self.to_phonetics()
+        return self.fmsg
+
+    def to_phonetics(self) -> None:
         """
         Turns a PletLang message into its phonetic equivalent.
         """
         def pmsg_delete(num_chars):
-            self.__pmsg = self.__pmsg[num_chars:]
+            self.pmsg = self.pmsg[num_chars:]
 
         def interpret_brackets():
-            global message_layers
-            if self.__pmsg.startswith("{{"):
+            if self.pmsg.startswith("{{"):
                 self.fmsg += "{*"
                 pmsg_delete(2)
-                self.__message_layers.append("longhand")
                 return read_longhand
-            elif self.__pmsg.startswith("{"):
+            elif self.pmsg.startswith("{"):
                 self.fmsg += "{"
                 pmsg_delete(1)
-                self.__message_layers.append("shorthand")
                 return read_shorthand
-            elif self.__pmsg.startswith("}}"):
+            elif self.pmsg.startswith("}}"):
                 self.fmsg += "*}"
                 pmsg_delete(2)
-                self.__message_layers.pop(-1)
-            elif self.__pmsg.startswith("}"):
+            elif self.pmsg.startswith("}"):
                 self.fmsg += "}"
                 pmsg_delete(1)
-                self.__message_layers.pop(-1)
             return "BACK"
             # Return previous message layer type
             # prev_layer = message_layers[-1]
@@ -53,33 +55,34 @@ class PletLangMessage:
             # else:
             #     return read_shorthand
 
-        def read_shorthand():
-            next_form = None
-            print("e")
+        def read_char():
+            # Check char's transliteration
+            if self.pmsg.startswith((",", "'")):
+                self.fmsg += self.pmsg[:1]
+                pmsg_delete(1)
+            else:
+                char_transliteration = self.pmsg[:4]
+                self.fmsg += phon_chars[char_transliteration]
+                pmsg_delete(4)
 
-            while self.__pmsg:
-                if self.__pmsg.startswith(BRACKETS):
+        def read_shorthand():
+            while self.pmsg:
+                if self.pmsg.startswith(BRACKETS):
                     # Interpret where the message will go next based on brackets
                     next_form = interpret_brackets()
                     if next_form == "BACK":
                         return
                     else:
                         next_form()
-                # Check char's transliteration
-                elif self.__pmsg.startswith((",", "'")):
-                    self.fmsg += self.__pmsg[:1]
-                    pmsg_delete(1)
-                else:
-                    char_transliteration = self.__pmsg[:4]
-                    self.fmsg += phon_chars[char_transliteration]
-                    pmsg_delete(4)
+                    # Char transliteration
+                    read_char()
 
         def read_longhand():
             # Read "{{1101 00000101" as a w in magenta; 0000 is the null color and 1101 is its default.
-            default_color = self.__pmsg[:4]
+            default_color = self.pmsg[:4]
             pmsg_delete(4)
-            while self.__pmsg:
-                if self.__pmsg.startswith(BRACKETS):
+            while self.pmsg:
+                if self.pmsg.startswith(BRACKETS):
                     # Interpret where the message will go next based on brackets
                     next_form = interpret_brackets()
                     if next_form == "BACK":
@@ -87,27 +90,17 @@ class PletLangMessage:
                     else:
                         next_form()
                 # Check char's color
-                char_color = self.__pmsg[:4]
+                char_color = self.pmsg[:4]
                 if char_color == "0000":
                     self.fmsg += ANSI_COLORS[default_color]
                 else:
                     self.fmsg += ANSI_COLORS[char_color]
                 pmsg_delete(4)
-                # Check char's transliteration
-                if self.__pmsg.startswith((",", "'")):
-                    self.fmsg += self.__pmsg[:1]
-                    pmsg_delete(1)
-                else:
-                    char_transliteration = self.__pmsg[:4]
-                    self.fmsg += phon_chars[char_transliteration]
-                    pmsg_delete(4)
+                # Char transliteration
+                read_char()
                 # Add color ender
                 self.fmsg += ANSI_COLORS["END"]
 
         # Start off by reading the first bracket.
         base_form = interpret_brackets()
         base_form()
-        return self.fmsg
-
-p = PletLangMessage("{ 0100 {{1011 00000101 11001001 0110' }} 0000 }")
-print(p.to_phonetics())
--- translator.py
+++ translator.py
@@ -1,15 +1,5 @@
 from plet_lang_input import PletLangInput
 from plet_lang_message import PletLangMessage
-import os
-
-os.system("color")
-
-for color in range(30, 37):
-    print(f"\x1b[{color}mHello!\x1b[0m")
-for color in range(90, 97):
-    print(f"\x1b[{color}mHello!\x1b[0m")
-
-print("\x1b[38;2;255;255;0mHello world!\x1b[0m")
 
 if __name__ == "__main__":
     # PletLang message input object
@@ -17,7 +7,9 @@ if __name__ == "__main__":
     # plet_msg = PletLangInput().__repr__()
     # print(plet_msg.to_phonetics())
     pmsg = PletLangMessage("{ 0100 {{1101 00000101 10101011 }} 1010 }")
-    pmsg.to_phonetics()
+    message = pmsg.get_phonetics()
 
     # Phonetics message input object
     # Process message with msg.to_plet_lang()
+
+print(message)
